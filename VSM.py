1ceb4c7e1206fd4607bd7721c03c8987b3150ea1
flyiyou <irooper@163.com>
2018-11-05 00:50:30 +0800

    first_commit

--- VSM.py
+++ VSM.py
@@ -2,8 +2,9 @@ import os
 import math
 from textblob import TextBlob
 from textblob import Word
+import re
 #获取文件
-def Get_Split_File (path):
+def Get_File (path):
     """
     path：文件路径
     return: 返回二维列表
@@ -16,26 +17,35 @@ def Get_Split_File (path):
         textdict = {}
         filedict[file] = {}    #每个文件夹下text列表
         for text in textfile: #遍历文件夹下每个text
-            #IFdict = {}
+            #TFdict = {}
             textdict[text] = {}
         filedict[file]=textdict    
     return filedict
 
-#分词并计算IF
+#分词并计算TF
 def Split_Static(filedict,path):
     for file in filedict.keys():
         for text in filedict[file].keys():
-            IFdict = {}
+            TFdict = {}
             filedict[file][text] = {}
-            worddata = open(path + "/" + file + "/" + text,"r",encoding="ISO-8859-1")
-            lines=worddata.readlines()
+            worddata = open(path + "/" + file + "/" + text,"rb")
+            text_t = worddata.read().decode('utf-8','ignore')
+            textlow = text_t.lower()
+            preword = re.sub('[^a-zA-Z]',' ',textlow)  #去除非字母字符
+            words = preword.split()
+            for word in words:
+                if not (word in TFdict):
+                        TFdict[word]=1
+                else :
+                    TFdict[word]+=1
+            '''lines=worddata.readlines()
             for line in lines:
                 line = line.lower()
                 line_replace = TextBlob(str(line).replace("\\\\n", " ").replace("\\\\t", " ").replace("\\"," ").replace("'"," ").replace("/"," "))
                 for word in line_replace.words:
-                    if not (word in IFdict):
-                        IFdict[word]=line_replace.words.count(word)
-            filedict[file][text] = IFdict
+                    if not (word in TFdict):
+                        TFdict[word]=1+math.log10(line_replace.words.count(word))'''
+            filedict[file][text] = TFdict
     return filedict
 #计算IDF
 def IDF(filedict,dict):
@@ -49,62 +59,73 @@ def IDF(filedict,dict):
     print(len(dict))
     with open("./data"+"/"+"all_fre"+".txt", "w+", encoding="utf-8") as output:
 	    for key in dict.keys():
-		    output.write(key + ":" + str(dict[key]) + "\n")
+		    output.write(key + ":" + str(dict[key]) + "\n")   #保存IDF
     for key in dict.keys():
         idf = math.log10(18829/dict[key])
         dict[key]=idf
 
-#计算IF*IDF并筛选词典
-def IF_IDF(path,filedict,dict):
+#计算TF*IDF并筛选词典,返回更新后的词典
+def TF_IDF(path,filedict,dict):
+    fin_dict = {}
     dict_t = {}
     for key in dict.keys():
         dict_t[key]=0
     for file in filedict.keys():
         for text in filedict[file].keys():
-            for word in list(filedict[file][text]):
+            for word in list(filedict[file][text]):  #遍历到单词
                 if word in dict:
-                    IF=filedict[file][text][word]
-                    IFIDF=IF*dict[word]
-                    filedict[file][text][word]=IFIDF
-                    if IFIDF<=1.2:          #########################
+                    TF=filedict[file][text][word]
+                    TFIDF=TF*dict[word]
+                    filedict[file][text][word]=TFIDF
+                    if (TFIDF >=15)and not(word in fin_dict):  #加到新词典
+                        fin_dict[word]=dict[word]
+                    '''if TFIDF>=2:          #########################
                         filedict[file][text].pop(word)
                         dict.pop(word)
                     else :
-                        dict[word]=IFIDF
+                        dict[word]=TFIDF '''
             '''with open(path+"/"+file+"/"+text+"_frequence.text", "w+", encoding="utf-8") as output:
                 for key in dict.keys():
                     output.write(key + ":" + str(dict[key]) + "\n")'''
-    print(len(dict))
+
+    with open("./data"+"/"+"final_dict"+".txt", "w+", encoding="utf-8") as output:
+	    for key in fin_dict.keys():
+		    output.write(key +"\n")      #保存最终词典
+    print(len(fin_dict))
+    return fin_dict
 #生成向量空间并保存，每个文本一个
 def Build_VSM(path,filedict,dict):
-    os.mkdir(path)
-    dict_t = {}
+    if not os.path.exists(path):
+        os.mkdir(path)
+    #dict_t = {}
     for file in filedict.keys():
         filepath = path +"/" +file
-        os.mkdir(filepath)
+        if not os.path.exists(filepath):
+            os.mkdir(filepath)
+        #for key in dict.keys():
+            #dict_t[key] = 0
         for text in filedict[file].keys():
-            for key in dict.keys():
-                dict_t[key] = 0
-            for word in filedict[file][text].keys():
-                if word in dict_t:
-                    dict_t[word]=1
-            with open(filepath+"/"+text+"_fre.text", "w+", encoding="utf-8") as output:
+            '''for word in filedict[file][text].keys():
+            if word in dict_t:
+                dict_t[word]=1 '''
+            with open(filepath+"/"+text+"_fre.text", "w", encoding="utf-8") as output:
                 for key in dict.keys():
-                    output.write(key + ":" + str(dict[key]) + " ")    
-
-            
-            
+                    if key in filedict[file][text]:
+                        output.write("1 ") 
+                    else:
+                        output.write("0 ")
 
 
 if __name__ == '__main__':                   
     path='./data/20news-18828'
-    mkdir_path='.data/20news_fre'
-    dict = {}
-    Filedict = Get_Split_File(path)
+    mkdir_path='./data/20news_fre'
+    raw_dict = {}
+    final_dict = {}
+    Filedict = Get_File(path)
     Split_Static(Filedict,path)
-    IDF(Filedict,dict)
-    IF_IDF(path,Filedict,dict)
-    Build_VSM(mkdir_path,Filedict,dict)
+    IDF(Filedict,raw_dict)
+    final_dict=TF_IDF(path,Filedict,raw_dict)
+    Build_VSM(mkdir_path,Filedict,final_dict)
 
 
                 
